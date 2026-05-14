19aedc755912266f1cdd23b2c0438bfded6ffcbb
Priya Yelve <your-real-email@gmail.com>
2026-05-14 15:17:56 -0600

    Added environment variables and deployment setup

--- app/config.py
+++ app/config.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///mechanic_shop.db"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
